ef0265604c105cce258fe1d49ca55459a212e81c
xOAviOx <avishuklacode@gmail.com>
2025-01-14 22:17:49 +0530

    added a debugger function

--- src/js/model.js
+++ src/js/model.js
@@ -107,4 +107,7 @@ const init = function () {
   if (storage) state.bookmarks = JSON.parse(storage);
 };
 init();
-console.log(state.bookmarks);
+
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
