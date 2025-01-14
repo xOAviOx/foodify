7a42757d0eaa9a84898acbbc4b4213a3b1af9208
xOAviOx <avishuklacode@gmail.com>
2025-01-14 11:33:55 +0530

    New files

--- src/js/model.js
+++ src/js/model.js
@@ -1,14 +1,12 @@
+import { API_URL } from "./config.js";
+import { getJSON } from "./helpers.js";
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+    const data = await getJSON(`${API_URL}/${id}`)
 
     const { recipe } = data.data;
     state.recipe = {
@@ -23,6 +21,7 @@ export const loadRecipe = async function (id) {
     };
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    // temp error handling
+   console.log(`${err}!!!!!`);
   }
 };
