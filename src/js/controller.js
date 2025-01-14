f5138ec65afed4eb6d2a014f6ed2fe82da5fcb29
xOAviOx <avishuklacode@gmail.com>
2025-01-14 18:57:33 +0530

    creating a virtual dom

--- src/js/controller.js
+++ src/js/controller.js
@@ -60,7 +60,7 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   //update the recipe view
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- src/js/views/View.js
+++ src/js/views/View.js
@@ -3,12 +3,23 @@ import icons from '../../img/icons.svg';
 export default class View {
   _data;
   render(data) {
-    if (!data || (Array.isArray(data) && data.length === 0)){
-      return this.renderError();}
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+      return this.renderError();
+    }
     this._data = data;
     const markup = this._generateMarkup();
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+      return this.renderError();
+    }
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
   }
 
   _clear() {
